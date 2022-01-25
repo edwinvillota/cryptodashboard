db2467c0a145bc78ed76075d264803a41a040b70
Edwin <evillota@technisys.com>
2022-01-24 22:57:21 -0500

    :write_check_mark: Added button unitary tests

--- src/components/atoms/Button/__tests__/Button.test.tsx
+++ src/components/atoms/Button/__tests__/Button.test.tsx
@@ -7,3 +7,15 @@ test('Should be rendered', () => {
   const button = screen.getByRole('button');
   expect(button).toBeInTheDocument();
 });
+
+test('Should be render a custom children', () => {
+  render(<Button>Test</Button>);
+  const children = screen.getByText('Test');
+  expect(children).toBeInTheDocument();
+});
+
+test('Should has a custom class', () => {
+  render(<Button className="customClass">Button</Button>);
+  const button = screen.getByRole('button');
+  expect(button).toHaveClass('customClass');
+});
